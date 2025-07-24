f758e9cf8ec65b35cb02a9368f17c8ce32030d89
RajBDevops <144301897+RajBDevops@users.noreply.github.com>
2025-07-24 16:52:14 +0530

    Update mydocker.sh

--- mydocker.sh
+++ mydocker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-if docker --version >/dev/null 2>&1;then
-  echo "docker is already installed"
-else 
+
   echo "docker is not installed, now install"
   sudo apt update -y
   sudo apt install docker.io -y
@@ -10,4 +8,4 @@ else
   sudo systemctl enable docker
   sudo usermod -aG docker ubuntu
   echo "docker has been installed"
-fi
+
