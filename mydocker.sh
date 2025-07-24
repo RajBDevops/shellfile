30bc679a638a8da81c3f86f7828d918b5c090d8f
RajBDevops <144301897+RajBDevops@users.noreply.github.com>
2025-07-24 10:49:53 +0530

    Create mydocker.sh

---
+++ mydocker.sh
@@ -0,0 +1,13 @@
+#!usr//bin/bash/sh
+if docker --version >/dev/null 2>&1;then
+  echo "docker is already installed"
+else 
+  echo "docker is not installed, now install"
+  sudo apt update -y
+  sudo apt install docker.io -y
+  docker --version
+  sudo systemctl start docker
+  sudo systemctl enable docker
+  sudo usermod -aG docker ubuntu
+  echo "docker has been installed"
+fi
