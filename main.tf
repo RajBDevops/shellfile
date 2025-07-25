eb29c5a6e4df98832e8f36862d2ae08bb6dc57aa
RajBDevops <144301897+RajBDevops@users.noreply.github.com>
2025-07-25 10:39:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "raj" {
-  instance_type = "t2.micro"
-  ami           = "ami-020cba7c55df1f615"
-  key_name      = "mahakey.pem"  
-
-  tags = {
-    Name = "myinstance"
-  }
-}
