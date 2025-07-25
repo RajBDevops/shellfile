b39e43c412785d526974b7223dbfbc0a269dd21e
RajBDevops <144301897+RajBDevops@users.noreply.github.com>
2025-07-25 11:37:03 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "raj" {
   instance_type = "t2.micro"
   ami           = "ami-020cba7c55df1f615"
-  key_name      = "mahakey.pem"  
-
+ 
   tags = {
     Name = "myinstance"
   }
